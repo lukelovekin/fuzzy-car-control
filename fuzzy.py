c5de33ddd25b16545566d6b22365c877acf8e264
lukelovekin <lukelovekin@gmail.com>
2022-03-17 18:23:46 +1000

    initial

---
+++ fuzzy.py
@@ -0,0 +1,36 @@
+"""
+Rules
+R1 x is this and y is this then z
+R2 x is this and y is this then z
+R3 x is this and y is this then z
+R4 x is this and y is this then z
+R5 x is this and y is this then z
+R6 x is this and y is this then z
+
+Partitions:
+Brake: Open left (a = 0, b = 100)
+Other: Open right ( a = 0, b = 100) 
+"""
+
+# import numpy as np
+
+# e.g variables
+velocity = 30
+speed = 30
+accelerate = 30
+
+# e.g prints to console
+print("The speed/velocity/acceleration input is: ...",)
+print("distance from car in front is: ...")
+print("distance from the car behind is: ...")
+print("\n")
+
+# def accelerate(value)
+
+# def brake(value)
+
+
+
+# fuzzification function
+
+# fuzzy partition
